58a76ce4a248410617025c15e2e785bff02550d0
Jonata Pereira <jonata_alvespereira@hotmail.com>
2025-03-18 22:20:33 -0300

    Criando Classes e pacotes

---
+++ POOGaragem/src/main/java/POOVaga/Vaga.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POOVaga;
+
+/**
+ *
+ * @author Jona
+ */
+public class Vaga {
+    private int idVaga;
+    private String ocupada;
+    private String hoEntrada;
+
+    
+}
